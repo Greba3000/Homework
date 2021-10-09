a0fad6a4c46b0e4ab249befb510b5096c57f8798
Greba3000 <Greba3000@gmail.com>
2021-10-09 13:44:10 +0300

    Add file with task4

---
+++ AlexanderHreben/task4.py
@@ -0,0 +1,19 @@
+# Implement decorator for supressing exceptions. If exception not occure write log to console.
+
+def supressing_exceptions(func):
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except Exception as exc:
+            print(f'We had exception - {exc} {type(exc)}')
+    return wrapper
+
+
+@supressing_exceptions
+def func(num):
+    x = num / 0
+    print("I'm here!")  # how it print?
+
+
+if __name__ == '__main__':
+    func('f')
