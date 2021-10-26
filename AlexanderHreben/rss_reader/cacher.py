d383115f32bb87f699fffeff237ca596c3a12dee
Greba3000 <Greba3000@gmail.com>
2021-10-26 15:33:30 +0300

    Fix method _get_convert_data. Add docstrings. Remove comments

--- AlexanderHreben/rss_reader/cacher.py
+++ AlexanderHreben/rss_reader/cacher.py
@@ -1,4 +1,4 @@
-""" проверки на дату, форматы даты, структура жсона"""
+"""Storage module for Cacher class"""
 
 import json
 import logging
@@ -8,34 +8,35 @@ logger = logging.getLogger('app.cacher')
 
 
 class Cacher:
-    """  """
+    """
+    Class for saving and retrieving news from the repository (db.json)
+    if the variable source == None it means that we use all the sources available in the storage (db.json)
+    """
 
     def __init__(self, source):
         self.source = source
         self.cache_file_path = "db.json"
-
+        """
+        :param source: URL of the RSS resource
+        """
+        
     def cache(self, feed: dict):
         """
-
-        :param feed:
-        :return:
+        Saves news from rss_parser to the repository (db.json)
+        :param feed: dictionary containing news from rss_parser
         """
+        
         with open(self.cache_file_path, "r+", encoding="utf-8") as cache_file:
-            json_content = cache_file.read()  # str
-            json_dicts = json.loads(json_content)  # list
-
-            # print(type(json_dicts), json_dicts)
+            json_content = cache_file.read()
+            json_dicts = json.loads(json_content)
 
             url_list = []
             counter = 0
+            
             for elem in json_dicts:
                 url_list.append([key for key in elem.keys()])
-                # print('elem', type(elem), elem)
-
                 if self.source in elem:
                     for item in feed.get('item'):
-                        # print('item', type(item), item)
-                        # print(json_dicts[counter].get(self.source)[0].get('item'))
                         if item not in json_dicts[counter].get(self.source)[0].get('item'):
                             json_dicts[counter].get(self.source)[0].get('item').append(item)
                 counter += 1
@@ -44,60 +45,62 @@ class Cacher:
             if [self.source] not in url_list:
                 json_dicts.append({self.source: [feed]})
 
-            # print(type(json_dicts), json_dicts)
-
         with open(self.cache_file_path, "w", encoding="utf-8") as cache_file:
             json.dump(json_dicts, cache_file, indent=4, ensure_ascii=False)
 
-    def get_cache_data(self, date: int) -> dict:  # если ниче нету бросить ошибку и написать нету
+    def get_cache_data(self, date: int) -> dict:
         """
-
-        :param date:
-        :return: Dict contain
+        Creates dictionary of news from the repository (db.json) published on a specific date.
+        :param date: value date in %Y%m%d format
+        :return: dict containing news from repository (db.json)
         """
         out_dict_source = {'item': []}
         out_dict_all = {'item': []}
+        
         with open(self.cache_file_path, "r", encoding="utf-8") as cache_file:
-            json_content = cache_file.read()  # str
-            json_dicts = json.loads(json_content)  # list
-            # print(json_dicts)
+            json_content = cache_file.read()
+            json_dicts = json.loads(json_content)
+            
             if self.source:
                 counter_dict_source_pos = 0
-                for elem in json_dicts:  # элемент это словарь из листа со всеми словарями
-                    # print('----', elem)
-                    if self.source in elem:  # если юрл в ключе то работаем с этим словарем
-                        # print(json_dicts[counter].get(self.source)[0].get('item')) - list
-                        for item in json_dicts[counter_dict_source_pos].get(self.source)[0].get(
-                                'item'):  # итем это итем нужного нам словаря
-                            # print(item['pubDate'])
+                for elem in json_dicts:
+                    if self.source in elem:
+                        for item in json_dicts[counter_dict_source_pos].get(self.source)[0].get('item'):
                             if date == Cacher.get_convert_date(item['pubDate']):
                                 out_dict_source.get('item').append(item)
                     counter_dict_source_pos += 1
                 logger.debug(f'Out dict with source news from cache - {out_dict_source}')
-                # print(out_dict_source)
                 return out_dict_source
             else:
                 counter_dict_all_pos = 0
-                for elem in json_dicts:  # дик кажого юрла
-                    # print('----', elem)
+                for elem in json_dicts:
                     for key in elem.keys():
                         key_url = key
-                    # print(elem.get(key_url)[0].get('item'))
-                    for item in elem.get(key_url)[0].get('item'):  # итем словаря
-                        if date == Cacher.get_convert_date(item['pubDate']):
+                    for item in elem.get(key_url)[0].get('item'):
+                        if date == Cacher._get_convert_date(item['pubDate']):
                             out_dict_all.get('item').append(item)
                     counter_dict_all_pos += 1
-                # print(out_dict_all)
                 logger.debug(f'Out dict with all news from cache - {out_dict_all}')
                 return out_dict_all
 
     @staticmethod
-    def get_convert_date(date: str):
+    def _get_convert_date(date: str) -> int:
         """
+        Convert date to %Y%m%d format
+        :param date: date in RFC 822 format
+        :return: parsed date in %Y%m%d format
+        """
+        allowed_date_formats = [
+            "%a, %d %b %Y %H:%M:%S %z",
+            "%a, %d %b %Y %H:%M:%S %Z",
+            "%Y-%m-%dT%H:%M:%SZ",
+        ]
+        temp_date = None
 
-        :param date:
-        :return:
-        """
-        temp_date = datetime.strptime(date, "%a, %d %b %Y %H:%M:%S %z")
+        for date_format in allowed_date_formats:
+            try:
+                temp_date = datetime.strptime(date, date_format)
+            except ValueError:
+                pass
         parsed_date = int(f"{temp_date.year}{temp_date.month:02d}{temp_date.day:02d}")
         return parsed_date
