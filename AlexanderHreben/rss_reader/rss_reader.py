3e60589e2e022545891a9d3f9d4680c33fac7a5f
Greba3000 <Greba3000@gmail.com>
2021-10-27 20:39:48 +0300

    Fix imports

--- AlexanderHreben/rss_reader/rss_reader.py
+++ AlexanderHreben/rss_reader/rss_reader.py
@@ -1,8 +1,15 @@
 """Main module for working with APP"""
 
+import sys
+from pathlib import Path
+
 import logging
-from config import AppLogger
-from rss_parser import RssParser
+
+rss_reader_pkg_dir_path = str(Path(__file__).parent.resolve())
+sys.path.append(rss_reader_pkg_dir_path)
+
+from grebarss_reader.config import AppLogger
+from grebarss_reader.rss_parser import RssParser
 
 AppLogger.init_logger('app')
 logger = logging.getLogger("app.rss_reader")
@@ -15,7 +22,7 @@ def main():
         rss_parser.start()
     except Exception as exc_obj:
         logger.exception(f"Rss reader crashed from {exc_obj}")
-        # print(f"Rss reader crashed from {type(exc_obj).__name__}")
+        print(f"Rss reader crashed from {type(exc_obj).__name__}. For more information see log file.")
 
 
 if __name__ == "__main__":
